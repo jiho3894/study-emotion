43b4367c17b03d7675f8239f80e3f09da549eede
김지호 <crsn1111@gmail.com>
2022-06-06 23:41:25 +0900

    [docs] 문서 수정

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,11 +1,9 @@
 import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
-import { FullPage, Slide } from "react-full-page";
-import "../style/slide.css";
 
 const Img = styled.img`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   filter: brightness(60%);
 `;
 
@@ -14,6 +12,7 @@ const Container = styled.main`
   height: 100vh;
   background-color: #1c2632;
   position: relative;
+  overflow-y: hidden;
 `;
 
 const Section = styled.section`
@@ -42,22 +41,17 @@ const Home = () => {
     return () => clearInterval(interval);
   });
   return (
-    <FullPage controls controlProps={{ className: "slide-navigation" }}>
-      <Slide>
-        <Container>
-          <Img
-            src="https://velog.velcdn.com/images/jiho3894/post/a85f26d1-92db-405c-a36a-8f024d2ffde3/image.png"
-            alt=""
-          />
-          <Section>
-            <span>{Text}</span>
-          </Section>
-        </Container>
-      </Slide>
-      <Slide>
-        <Container></Container>
-      </Slide>
-    </FullPage>
+    <>
+      <Container>
+        <Img
+          src="https://velog.velcdn.com/images/jiho3894/post/a85f26d1-92db-405c-a36a-8f024d2ffde3/image.png"
+          alt=""
+        />
+        <Section>
+          <span>{Text}</span>
+        </Section>
+      </Container>
+    </>
   );
 };
 
